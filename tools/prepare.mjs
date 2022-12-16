cc2b16687c839efdc6fcdd35bd89589d5582fa0e
BOTREL Kilian <botrel.kilian1999@gmail.com>
2022-12-16 22:05:23 +0100

    feat: project configuration

---
+++ tools/prepare.mjs
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import { readFile, writeFile } from 'node:fs/promises'
+import process from 'node:process'
+
+import husky from 'husky'
+
+const isProductionEnvironment = process.env.NODE_ENV === 'production'
+
+if (!isProductionEnvironment) {
+  husky.install()
+
+  const filePath = '.husky/_/husky.sh'
+
+  const file = await readFile(filePath, 'utf8')
+  const updatedFile = file.replace(/sh/g, 'bash')
+  const array = []
+
+  await writeFile(filePath, updatedFile)
+}
